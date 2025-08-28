32e51866d17529be2b892d5ce62fd676866d71a8
Aaditya7171 <risingaditya91@gmail.com>
2025-08-28 17:34:44 +0530

    update

--- Section 23 - Node.js/2.2+Native+Modules/2.2 Native Modules/index.js
+++ Section 23 - Node.js/2.2+Native+Modules/2.2 Native Modules/index.js
@@ -16,7 +16,7 @@ const fs = require("fs");
 // });
 
 
-fs.readFile("./message.txt", "utf-8", (err, data)=> {
-    if(err) throw err;
+fs.readFile("./message.txt", "utf-8", (err, data) => {
+    if (err) throw err;
     console.log(data);
-})
+}) 
